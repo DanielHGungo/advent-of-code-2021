2cff259369982edf09c5c42351da45569cdebf41
DanielHGungo <gungoblue@gmail.com>
2021-12-17 23:13:02 -0800

    finished day10 part 2

--- adventofcode/day10/2part.js
+++ adventofcode/day10/2part.js
@@ -5,49 +5,84 @@ const myInput = fs
 .split("\r\n")
 .filter(Boolean);
 
-function countUnexpected(arr){
-  let wrongArray = [];
-  let wrongChar;
+function getPointsArray(arr){
+  let properCloseArr = [];
+  let curArr;
   for(let i=0; i<arr.length; i++){  
-    wrongChar = getWrongClose(arr[i]);
-    if(wrongChar !== 'good'){
-        wrongArray.push(wrongChar);
+    curArr = getProperClose(arr[i]);
+    if(curArr.length > 0){
+      properCloseArr.push(curArr);
     }
   }
-  return countPoints(wrongArray);    
+  let pointsArr = new Array(properCloseArr.length);
+  for(let j=0; j<properCloseArr.length; j++){
+    pointsArr[j] = countPoints(properCloseArr[j]);
+  }
+  return pointsArr;
 }
 function countPoints(arr){
   let count = 0;
   for(let i=0; i<arr.length; i++){
-    if(arr[i] === ')')
-    count += 3;
-    else if(arr[i] === '}')
-    count += 1197;
-    else if(arr[i] === ']')
-    count += 57;
-    else
-    count += 25137;
+    count = count * 5;
+    switch(arr[i]){
+      case ')': count += 1; break;
+      case ']': count += 2; break;
+      case '}': count += 3; break;
+      case '>': count += 4; break;
+    }
   }
   return count;
 }
-function getWrongClose(str){
+function getProperClose(str){
   let myStr = str;
+  let isWrongClose = false;
   let myArr = myStr.split('');
+  let badReturn = [];
   let openArr = [];
   for(let i=0; i<myArr.length; i++){
     let ai = myArr[i];
     if(ai === '(' || ai === '[' || ai === '{' || ai === '<')
     openArr.push(ai);
     else {
-      if(openArr.length < 1) { return ai; }
+      if(openArr.length < 1) {
+        isWrongClose = true;
+        break;
+      }
       let oP = getOpenCounterPart(ai);
       if(openArr[openArr.length - 1] === oP)
       openArr.pop();
-      else
-      return ai;
+      else{
+        isWrongClose = true;
+        break;
+      }
     }
   }
-  return 'good';
+  if(isWrongClose){
+    return badReturn;
+  } else {
+    return flipOpenArray(openArr);
+  }
+}
+function flipOpenArray(arr){
+  let reArr = [];
+  for(let i=arr.length-1; i >= 0; i--){
+    reArr.push(getCloseCounterPart(arr[i]));
+  }
+  return reArr;
+}
+function getCloseCounterPart(char){
+  switch(char){
+    case '(':
+        return ')';
+    case '{':
+        return '}';
+    case '[':
+        return ']';
+    case '<':
+        return '>';
+    default:
+        return null;
+  }  
 }
 function getOpenCounterPart(char){
   switch(char){
@@ -63,7 +98,20 @@ function getOpenCounterPart(char){
         return null;
   }
 }
+//assume odd number in array.
+function getMidPoint(arr){
+  // console.log(arr);
+  if(arr.length % 2 === 0) {console.log('this was an even Array');}
+  let myArr = arr.sort(function(a, b) {
+    return a - b;
+  });
+  // console.log(myArr);
+  let mid = Math.floor(myArr.length / 2);
+  return myArr[mid];
+}
+
 // console.dir(myInput, {'maxArrayLength': null});
 //////////CALLS////////////////////////////////
-let num = countUnexpected(myInput);
-console.log('the answer value is:  '+ num);
+let ansArr = getPointsArray(myInput);
+let answer = getMidPoint(ansArr);
+console.log('the midpoint high score is:  '+ answer);
